06241804c76c31cd5b2aef51899cf9a9e39cd316
gabfdomingues <gabriella.domingues80@gmail.com>
2025-03-10 23:42:49 +0000

    codigos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,15 +2,35 @@
 
 int main() {
 
-    printf ("Cartas Super Trunfo!");
+    //Variáveis carta 1 
 
-    char estado = "Ceará";
-    char nome da cidade;
-    int população;
-    float área;
-    float pib;
-    int numero de pontos;
+    char codigo [4];
+    char estado [4];
+    char nome [20];
+    int populacao;
+    float area;
+    int pontos_turisticos;
 
-    printf ("estado: %s")
+    //Variáveis carta 2
+
+    char codigo2[4];
+    char estado[4];
+    char nome2 [4];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontos_turisticos2;
+
+    //Dados de cadastro da primeira carta
+
+    printf ("Codigo da primeira carta, digite codigo carta 1:\n")
+    scanf("%s", &codigo);
+
+    return 0
+
+
+
+
+
 
 }
