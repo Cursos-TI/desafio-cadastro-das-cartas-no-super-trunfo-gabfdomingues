260d03f8a592014c5f0d5416b57d92d44799a338
gabfdomingues <gabriella.domingues80@gmail.com>
2025-03-11 00:03:45 +0000

    cod carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -23,10 +23,27 @@ int main() {
 
     //Dados de cadastro da primeira carta
 
-    printf ("Codigo da primeira carta, digite codigo carta 1:\n")
-    scanf("%s", &codigo);
+    printf("Codigo da primeira carta, digite codigo carta 1:\n")
+    scanf("%s", &codigo1);
 
-    return 0
+    printf("digite o estado da carta 1: ");
+    scanf("%s, &estado1");
+
+    printf("nome da cidade carta 1, ");
+    scanf("%s", &nome1);
+
+    printf("População carta 1:");
+    scanf("%d", &populacao)1;
+
+    printf("area da carta 1:");
+    scanf("%f", &area1);
+
+    printf("pib da carta 1:");
+    scanf("%f", &pib1);
+
+    print("pontos turísticos carta 1:");
+    scanf("%d" , &pontos_turisticos1);
+
 
 
 
